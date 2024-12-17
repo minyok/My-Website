15f5beae32482a2811ec5041b7ff7dcac135ed83
minyok <78887087+minyok@users.noreply.github.com>
2024-12-17 19:18:34 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navbar = document.getElementById('navbar');
-
-// Toggle navbar visibility with animation
-hamburger.addEventListener('click', () => {
-  navbar.classList.toggle('active');
-});
